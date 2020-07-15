14ea5e40b85ad6356ffeb32792c67c8059a192a3
Wayne Wilcox <wilcox_wayne@hotmail.com>
2020-07-15 19:47:39 -0400

    Co-authored-by: howaboutjeho <jac783@nyu.edu> Co-authored-by: geewailee <geewai.lee@gmail.com> Co-authored-by: Eelan <eelantung@outlook.com>

--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -4,29 +4,23 @@ export const Context = React.createContext();
 
 export const MyProvider = (props) => {
   const initialState = {
-    list: [
-      {
-        itemId: 1,
-        task: "Add the delete functionality",
-        completed: false,
-      },
-    ],
+    uiItems: [],
   };
 
   const reducer = (state, action) => {
     switch (action.type) {
       case "add_uis":
-        console.log(action);
         setUiItems(action.payload);
-        return state;
+        return {...state, uiItems: action.payload};
       default:
         return state;
     }
   };
-
+  // not using 21 - 23 until stretch 
   const [UiItems, setUiItems] = useState("");
   const [user, setUser] = useState("");
   const [organization, setOrganization] = useState("");
+
   const [globalState, dispatch] = React.useReducer(reducer, initialState);
   console.log(globalState);
   return (
