8955af0ee2ef059f32014ac1270cfe98b443453a
Cameron Fitz <hellocameronfitz@gmail.com>
2020-07-15 10:29:32 -0400

    My provider enabled

--- src/components/App.js
+++ src/components/App.js
@@ -12,35 +12,35 @@ const pool = new Pool({ connectionString: PG_URI });
 class App extends Component {
 	render() {
 		return (
-			// <MyProvider>
-			<Router>
-				<div className="navbar">
-					<ul>
-						<li>
-							<Link to="/">UI Library</Link>
-						</li>
-						<li>
-							<Link to="/generator">UI Generator</Link>
-						</li>
-						<li>
-							<Link to="/detailPage">Detail Page</Link>
-						</li>
-					</ul>
-				</div>
-				{/* a SWITCH statement to look through all ROUTE elements*/}
-				<Switch>
-					<Route exact path="/">
-						<UiLibrary />
-					</Route>
-					<Route exact path="/generator">
-						<Generator />
-					</Route>
-					<Route exact path="/detailPage">
-						<DetailPage />
-					</Route>
-				</Switch>
-			</Router>
-			// </MyProvider>
+			<MyProvider>
+				<Router>
+					<div className="navbar">
+						<ul>
+							<li>
+								<Link to="/">UI Library</Link>
+							</li>
+							<li>
+								<Link to="/generator">UI Generator</Link>
+							</li>
+							<li>
+								<Link to="/detailPage">Detail Page</Link>
+							</li>
+						</ul>
+					</div>
+					{/* a SWITCH statement to look through all ROUTE elements*/}
+					<Switch>
+						<Route exact path="/">
+							<UiLibrary />
+						</Route>
+						<Route exact path="/generator">
+							<Generator />
+						</Route>
+						<Route exact path="/detailPage">
+							<DetailPage />
+						</Route>
+					</Switch>
+				</Router>
+			</MyProvider>
 		);
 	}
 }
