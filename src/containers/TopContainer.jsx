af11d136e2878d0307f7ca9e4b856be9b244fdfe
Wayne Wilcox <wilcox_wayne@hotmail.com>
2020-07-25 17:18:47 -0400

    implemented exporting capabilities

--- src/containers/TopContainer.jsx
+++ src/containers/TopContainer.jsx
@@ -1,9 +1,24 @@
 import "../assets/css/TopContainer.css";
 import React from "react";
 import BuildItem from "../components/BuildItem.jsx";
+import createFiles from "../utils/createFiles.js";
+
+const IPC = require('electron').ipcRenderer;
 
 const TopContainer = (props) => {
+  IPC.on('app_dir_selected', (event, path) => {
+    console.log(`event ${event}`)
+    console.log(path)
+
+    // call createFiles(components, path)
+  });
+
   const { items } = props;
+
+  function handleDownload() {
+    console.log('handleDownload')
+    IPC.send('choose_app_dir')
+  }
 
   return (
     <div className="topContainer">
@@ -12,6 +27,9 @@ const TopContainer = (props) => {
         items.map((item) => (
           <BuildItem id={item.id} key={item.id} item={item} />
         ))}
+        <div className="downloadButton">
+          <button onClick={handleDownload} >Download</button>
+        </div>
     </div>
   );
 };
